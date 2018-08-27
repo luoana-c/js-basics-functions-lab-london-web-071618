9144b141f6cdcadaece131d5f6d6a5f2315a6dd7
Luoana Chirita <luoana.livadariu@gmail.com>
2018-08-27 09:16:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(address) {
+  if (address > 42) {
+    distance = address - 42
+  }
+  else {
+    distance = 42 - address
+  }
+  return distance
+}
+
+function distanceFromHqInFeet(address) {
+  distance = distanceFromHqInBlocks(address) * 264
+  return distance 
+}
+
+function distanceTravelledInFeet(startAddress, endAddress) {
+  if (endAddress > startAddress) { 
+    distance = (endAddress - startAddress) * 264
+  }
+  else {
+    distance = (startAddress - endAddress) * 264 
+  }
+  
+  return distance 
+}
+
+function calculatesFarePrice(start, destination) {
+  distance = distanceTravelledInFeet(start, destination)
+  
+  if (distance <= 400) {
+    price = 0
+  }
+  else if (distance > 400 && distance <= 2000) {
+    price = 0.02 * (distance - 400)
+  }
+  
+  else if (distance > 2000 && distance <= 2500) {
+    price = 25
+  }
+  
+  else  {
+    price = "cannot travel that far"
+  }
+  
+  return price
+}
